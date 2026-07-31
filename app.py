c84c9eba03565aa1a5e5d06b66118533b06c45dc
NUBI-AK <Nubicrypt01@gmail.com>
2026-07-31 17:55:47 -0400

    test1

--- app.py
+++ app.py
@@ -8,18 +8,24 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
-    'sqlite:///' + os.path.join(basedir, 'createbeats.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'False')
 app.config['SITE_URL'] = os.environ.get('SITE_URL')
 
 db.init_app(app)
 
+# Get the absolute path of the project directory
+  # Load environment variables from .env file
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 # Use environment variable if present, otherwise fall back to the explicit sqlite path
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+    'sqlite:///' + os.path.join(basedir, 'createbeats.db')
 
 import routes
 routes.init_app(app)
