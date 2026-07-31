00e275ab4335d0fc46847263d6e903307c4fb5e2
NUBI-AK <Nubicrypt01@gmail.com>
2026-07-31 17:58:42 -0400

    test2

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from models import db, Beat
 import os
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# load_dotenv(os.path.join(basedir, '.env'))
+load_dotenv()
 
 
 app = Flask(__name__)
